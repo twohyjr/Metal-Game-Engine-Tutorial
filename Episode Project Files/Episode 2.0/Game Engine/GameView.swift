60c2a61df99e5b593c7dc70943dcf178cd378640
Rick Twohy <twohyjr@gmail.com>
2018-03-29 12:29:07 -0500

    Initial Commit

---
+++ Episode Project Files/Episode 2.0/Game Engine/GameView.swift
@@ -0,0 +1,54 @@
+import MetalKit
+
+class GameView: MTKView {
+    
+    var commandQueue: MTLCommandQueue!
+    var renderPipelineState: MTLRenderPipelineState!
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.device = MTLCreateSystemDefaultDevice()
+        
+        self.clearColor = MTLClearColor(red: 0.43, green: 0.73, blue: 0.35, alpha: 1.0)
+        
+        self.colorPixelFormat = .bgra8Unorm
+        
+        self.commandQueue = device?.makeCommandQueue()
+        
+        createRenderPipelineState()
+    }
+    
+    func createRenderPipelineState(){
+        let library = device?.makeDefaultLibrary()
+        let vertexFunction = library?.makeFunction(name: "basic_vertex_shader")
+        let fragmentFunction = library?.makeFunction(name: "basic_fragment_shader")
+        
+        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
+        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        renderPipelineDescriptor.vertexFunction = vertexFunction
+        renderPipelineDescriptor.fragmentFunction = fragmentFunction
+        
+        do{
+            renderPipelineState = try device?.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+        }catch let error as NSError{
+            print(error)
+        }
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        guard let drawable = self.currentDrawable, let renderPassDescriptor = self.currentRenderPassDescriptor else { return }
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        renderCommandEncoder?.setRenderPipelineState(renderPipelineState)
+        
+        //Send info to rendercommandencoder
+        
+        renderCommandEncoder?.endEncoding()
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
+    }
+    
+}
+
---
+++ Full Current Project/Game Engine/Game Engine/MyShaders.metal
@@ -0,0 +1,13 @@
+
+#include <metal_stdlib>
+using namespace metal;
+
+
+vertex float4 basic_vertex_shader(){
+    return float4(1);
+}
+
+fragment half4 basic_fragment_shader(){
+    return half4(1);
+}
+
