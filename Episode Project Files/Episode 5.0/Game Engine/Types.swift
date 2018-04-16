6991729928940a26545fcf016ba8abb8edb00646
Rick Twohy <twohyjr@gmail.com>
2018-04-15 20:28:45 -0500

    Episode 5 Code Done

---
+++ Episode Project Files/Episode 5.0/Game Engine/Types.swift
@@ -0,0 +1,32 @@
+import simd
+
+protocol sizeable{
+    static func size(_ count: Int)->Int
+    static func stride(_ count: Int)->Int
+}
+
+extension sizeable{
+    static func size()->Int{
+        return MemoryLayout<Self>.size
+    }
+    
+    static func stride()->Int{
+        return MemoryLayout<Self>.stride
+    }
+    
+    static func size(_ count: Int)->Int{
+        return MemoryLayout<Self>.size * count
+    }
+    
+    static func stride(_ count: Int)->Int{
+        return MemoryLayout<Self>.stride * count
+    }
+}
+
+extension float3: sizeable { }
+
+struct Vertex: sizeable{
+    var position: float3
+    var color: float4
+}
+
