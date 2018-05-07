cbab0017a2463a0dab2e66f4e1c9b5fc59f9ef08
Rick Twohy <twohyjr@gmail.com>
2018-05-07 07:13:59 -0500

    Added ep 7 code

---
+++ Episode Project Files/Episode_7.0_GameObject/Game Engine/Game Engine/Libraries/RenderPipelineStateLibrary.swift
@@ -0,0 +1,43 @@
+import MetalKit
+
+enum RenderPipelineStateTypes {
+    case Basic
+}
+
+class RenderPipelineStateLibrary {
+    
+    private static var renderPipelineStates: [RenderPipelineStateTypes: RenderPipelineState] = [:]
+    
+    public static func initialize(){
+        createDefaultRenderPipelineStates()
+    }
+    
+    private static func createDefaultRenderPipelineStates(){
+        
+        renderPipelineStates.updateValue(Basic_RenderPipelineState(), forKey: .Basic)
+        
+    }
+    
+    public static func PipelineState(_ renderPipelineStateType: RenderPipelineStateTypes)->MTLRenderPipelineState{
+        return (renderPipelineStates[renderPipelineStateType]?.renderPipelineState)!
+    }
+    
+}
+
+protocol RenderPipelineState {
+    var name: String { get }
+    var renderPipelineState: MTLRenderPipelineState { get }
+}
+
+public struct Basic_RenderPipelineState: RenderPipelineState {
+    var name: String = "Basic Render Pipeline State"
+    var renderPipelineState: MTLRenderPipelineState {
+        var renderPipelineState: MTLRenderPipelineState!
+        do{
+            renderPipelineState = try Engine.Device.makeRenderPipelineState(descriptor: RenderPipelineDescriptorLibrary.Descriptor(.Basic))
+        }catch let error as NSError {
+            print("ERROR::CREATE::RENDER_PIPELINE_STATE::__\(name)__::\(error)")
+        }
+        return renderPipelineState
+    }
+}
