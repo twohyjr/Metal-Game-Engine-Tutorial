c51e5381459d0a415b024556d56aa03f97e1c024
Rick Twohy <twohyjr@gmail.com>
2018-06-11 19:24:01 -0500

    Added Separate Episode Files

---
+++ Episode Project Files/Episode_10.0_SceneGraphs/Game Engine/Core/Engine.swift
@@ -0,0 +1,27 @@
+import MetalKit
+
+class Engine {
+    
+    public static var Device: MTLDevice!
+    public static var CommandQueue: MTLCommandQueue!
+    
+    public static func Ignite(device: MTLDevice){
+        self.Device = device
+        self.CommandQueue = device.makeCommandQueue()
+        
+        ShaderLibrary.Initialize()
+        
+        VertexDescriptorLibrary.Intialize()
+        
+        RenderPipelineDescriptorLibrary.Initialize()
+        
+        RenderPipelineStateLibrary.Initialize()
+        
+        MeshLibrary.Initialize()
+        
+        
+        SceneManager.Initialize(Preferences.StartingSceneType)
+        
+    }
+    
+}
