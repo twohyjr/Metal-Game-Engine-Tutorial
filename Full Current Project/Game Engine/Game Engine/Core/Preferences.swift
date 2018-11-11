df162a054d2b23fff0b8b6174d0683d588eec4b8
twohyjr <twohyjr@gmail.com>
2018-11-11 18:34:50 -0500

    Sneak in one small change!

--- Full Current Project/Game Engine/Game Engine/Core/Preferences.swift
+++ Full Current Project/Game Engine/Game Engine/Core/Preferences.swift
@@ -5,6 +5,7 @@ public enum ClearColors{
     static let Green: MTLClearColor = MTLClearColor(red: 0.22, green: 0.55, blue: 0.34, alpha: 1.0)
     static let Grey: MTLClearColor = MTLClearColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
     static let Black: MTLClearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 1)
+    static let LimeGreen: MTLClearColor = MTLClearColor(red: 0.3, green: 0.7, blue: 0.3, alpha: 1) //Snuck this in :)
 }
 
 class Preferences {
