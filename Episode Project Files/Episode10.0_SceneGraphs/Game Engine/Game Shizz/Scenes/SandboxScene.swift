d354feee1a56c600895e1568990e8ba0bf25d102
Rick Twohy <twohyjr@gmail.com>
2019-04-12 00:02:21 -0400

    Updated Code To Swift 5

--- Episode Project Files/Episode10.0_SceneGraphs/Game Engine/Game Shizz/Scenes/SandboxScene.swift
+++ Episode Project Files/Episode10.0_SceneGraphs/Game Engine/Game Shizz/Scenes/SandboxScene.swift
@@ -2,19 +2,16 @@ import MetalKit
 
 class SandboxScene: Scene{
     
-
     override func buildScene() {
-        
         for y in -5..<5{
             for x in -5..<5{
                 let player = Player()
                 player.position.y = Float(Float(y) + 0.5) / 5
                 player.position.x = Float(Float(x) + 0.5) / 5
-                player.scale = float3(0.1)
+                player.scale = float3(0.1, 0.1, 0.1)
                 addChild(player)
             }
         }
-        
     }
     
     override func update(deltaTime: Float) {
