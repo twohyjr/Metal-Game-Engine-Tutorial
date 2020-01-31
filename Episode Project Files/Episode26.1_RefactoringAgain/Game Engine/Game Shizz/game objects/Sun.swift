0e6e353a79d03ce0f5a7dcc44bd3697c434a6652
Rick Twohy <twohyjr@gmail.com>
2020-01-31 12:12:47 -0500

    Forgot to remove sphere

--- Episode Project Files/Episode26.1_RefactoringAgain/Game Engine/Game Shizz/game objects/Sun.swift
+++ Episode Project Files/Episode26.1_RefactoringAgain/Game Engine/Game Shizz/game objects/Sun.swift
@@ -2,7 +2,7 @@ import simd
 
 class Sun: LightObject {
     init() {
-        super.init(name: "Sun", meshType: .Sphere)
+        super.init(name: "Sun")
         self.setMaterialColor(float4(0.5, 0.5, 0, 1.0))
         self.setScale(0.3)
     }
