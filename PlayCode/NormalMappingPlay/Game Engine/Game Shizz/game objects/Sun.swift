fcbd9600d1620d0f11a783a04b20d7f4c243edfb
Rick Twohy <twohyjr@gmail.com>
2020-07-17 19:35:49 -0400

    updated master with full current project

--- PlayCode/NormalMappingPlay/Game Engine/Game Shizz/game objects/Sun.swift
+++
@@ -1,9 +0,0 @@
-import simd
-
-class Sun: LightObject {
-    init() {
-        super.init(meshType: .Sphere, name: "Sun")
-        self.setMaterialColor(float4(1,1,1,1))
-        self.setScale(0.6)
-    }
-}
