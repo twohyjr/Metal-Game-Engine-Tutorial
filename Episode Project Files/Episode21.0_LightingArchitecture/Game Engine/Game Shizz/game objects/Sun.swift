51081816c19e6f1b19b7ef8cf03f2c0fd91e88d6
Rick Twohy <twohyjr@gmail.com>
2020-07-17 20:33:14 -0400

    Added Episode 29 Code

--- Episode Project Files/Episode21.0_LightingArchitecture/Game Engine/Game Shizz/game objects/Sun.swift
+++
@@ -1,9 +0,0 @@
-import simd
-
-class Sun: LightObject {
-    init() {
-        super.init(meshType: .Sphere, name: "Sun")
-        self.setColor(float4(0.5, 0.5, 0, 1.0))
-        self.setScale(0.3)
-    }
-}
